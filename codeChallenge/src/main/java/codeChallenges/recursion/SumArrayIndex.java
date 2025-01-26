0a93d7a686444cea70c102ae887e2e440c5b92d8
a.feiz <amirfeiz7@gmail.com>
2025-01-26 14:24:37 +0330

    SumArrayIndex: simple solution

--- codeChallenge/src/main/java/codeChallenges/recursion/SumArrayIndex.java
+++ codeChallenge/src/main/java/codeChallenges/recursion/SumArrayIndex.java
@@ -4,6 +4,8 @@ public class SumArrayIndex implements SumArrayIndexStrategy {
 
     @Override
     public long sumArrayIndex(int[] arr , int index) {
-        return 0;
+        if (index == 0) // base case
+            return arr[0];
+        return arr[index] + sumArrayIndex(arr, index - 1); // recursive case
     }
 }
